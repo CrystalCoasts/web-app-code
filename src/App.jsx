6edbbe937f3c9389cf3b5b38dcc5414fc219b3eb
lisette926 <84044778+lisette926@users.noreply.github.com>
2024-04-03 17:27:02 -0400

    New Oxygen Item

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./styles.css";
 import Papa from "papaparse";
 import PHScale from "./PHScale";
+import OxygenLevelCircle from "./OxygenLevelCircle";
 
 const Dashboard = () => {
   const [data, setData] = useState({});
@@ -10,7 +11,9 @@ const Dashboard = () => {
     const fetchData = () => {
       const googleDriveCsvUrl =
         "https://docs.google.com/spreadsheets/d/e/2PACX-1vSRWgtxHG2hLyrOTSPxgKrMzhGlkcqdyLV8tQobZa8B4Pah4giCjY-Ek5euXmK5hvxf3vvPLZTHfzJG/pub?gid=156216978&single=true&output=csv";
-      Papa.parse("./data.csv", {
+      const dataTestArduino =
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vR2WvNSmE8sFCEviG3keE78vd0vCAJJp5bC7NrV8LHCpyKr_8rcax51IpHHbUClx6bjAEeAeNYnQkrk/pub?output=csv";
+      Papa.parse(googleDriveCsvUrl, {
         download: true,
         delimeter: ",",
         complete: function (results) {
@@ -29,7 +32,7 @@ const Dashboard = () => {
     fetchData();
     const intervalID = setInterval(fetchData, 5000); //5 second intervals
 
-    // return () => clearInterval(intervalID);
+    return () => clearInterval(intervalID);
   }, []);
 
   return (
@@ -65,17 +68,37 @@ const Metric = ({ name, value }) => {
   console.log("Metric:", name, value);
   return (
     <div className="metric">
-      <h2>{name}</h2>
       {name === "pH" ? (
-        <>
-          <p>{value}</p>
-          <PHScale pH={value} /> {}
-        </>
+        <div className="metric-value">
+          <PHScale pH={value} />
+        </div>
+      ) : name === "Oxygen Level" ? (
+        <div className="metric-value">
+          <OxygenLevelCircle oxygenLevel={value} />
+        </div>
       ) : (
-        <p>{value}</p>
+        <p>
+          {value} {getUnitRate(name)}
+        </p>
       )}
+      <h2>{name}</h2>
     </div>
   );
+};
+
+const getUnitRate = (name) => {
+  switch (name) {
+    case "Salinity":
+      return "ppm"; // Parts per million
+    case "Turbidity":
+      return "NTU"; // Nephelometric Turbidity Units
+    case "TDS":
+      return "ppm"; // Parts per million
+    case "Nitride Conc.":
+      return "mg/L"; // Milligrams per liter
+    default:
+      return ""; // Default case
+  }
 };
 
 const Footer = () => {
